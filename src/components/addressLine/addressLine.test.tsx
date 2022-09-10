a3a43ba1fa27fa5a6d86aeb367be548eecc87e66
Thomas <thomas.morganshaw@gmail.com>
2022-09-11 00:26:17 +0100

    jest test

--- src/components/addressLine/addressLine.test.tsx
+++ src/components/addressLine/addressLine.test.tsx
@@ -21,3 +21,34 @@ test('render full address with all properties', () => {
   expect(linkElement[0]).toHaveTextContent(expectedString);
 
 });
+
+test('render minimal properties only shows postcode', () => {
+
+  var propertyResultWithPostcode = {
+    postcode: "AB1 2CD"
+  }
+
+  var expectedString = 'AB1 2CD'
+
+  render(<AddressLine propertyResult={propertyResultWithPostcode} />);
+  const linkElement = screen.getAllByRole('address-line') //recommended to add roles to components for accessiblity reasons
+  expect(linkElement[0]).toHaveTextContent(expectedString);
+
+});
+
+test('mixture of properties, only renders what has been provided', () => {
+
+  var propertyResultWithPostcode = {
+    houseNumber: 1,
+    addressLine1: "Some street",
+    town: "Someton",
+    postcode: "AB1 2CD"
+  }
+
+  var expectedString = '1, Some street, Someton, AB1 2CD'
+
+  render(<AddressLine propertyResult={propertyResultWithPostcode} />);
+  const linkElement = screen.getAllByRole('address-line') //recommended to add roles to components for accessiblity reasons
+  expect(linkElement[0]).toHaveTextContent(expectedString);
+
+});
